d40903f7099ce7468882f01a19385ff162c56a1d
Stephy <alves@stephanimoroni.com>
2017-06-21 00:49:02 -0700

    Add initial snapshot, hey! better than nothing

--- CalendarPicker/__tests__/CalendarPicker-test.js
+++ CalendarPicker/__tests__/CalendarPicker-test.js
@@ -1,7 +1,34 @@
 /* eslint-env jasmine */
 
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CalenderPicker from '../index';
+
+
 describe('CalendarPicker', function() {
-  it('works', function() {
-    expect(1).toBe(1);
+  it('It renders calendar picker', () => {
+    const CalendarPicker = renderer.create(
+      <CalenderPicker />
+    ).toJSON();
+    expect(CalendarPicker).toMatchSnapshot();
   });
+
+  it('It renders calendar picker with props', () => {
+    const minDate = new Date(); // Today
+    const maxDate = new Date(2017, 6, 3);
+    const CalendarPicker = renderer.create(
+      <CalenderPicker
+        startFromMonday={true}
+        allowRangeSelection={true}
+        minDate={minDate}
+        maxDate={maxDate}
+        todayBackgroundColor="#f2e6ff"
+        selectedDayColor="#7300e6"
+        selectedDayTextColor="#FFFFFF"
+        onDateChange={() => {}}
+      />
+    ).toJSON();
+    expect(CalendarPicker).toMatchSnapshot();
+  });
+
 });
