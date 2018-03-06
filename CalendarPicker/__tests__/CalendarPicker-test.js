70e7ec1f47538115faf9eb67472ae663b204d612
Andrew Puckett <apuckett7@gmail.com>
2018-03-06 11:12:04 -0600

    Fix Travis Test (#106)

--- CalendarPicker/__tests__/CalendarPicker-test.js
+++ CalendarPicker/__tests__/CalendarPicker-test.js
@@ -6,29 +6,32 @@ import CalenderPicker from '../index';
 
 
 describe('CalendarPicker', function() {
-  it('It renders calendar picker', () => {
-    const CalendarPicker = renderer.create(
-      <CalenderPicker />
-    ).toJSON();
-    expect(CalendarPicker).toMatchSnapshot();
-  });
+	it('It renders calendar picker', () => {
+		const CalendarPicker = renderer.create(
+			<CalenderPicker
+				initialDate={new Date(2017, 11, 31)}
+			/>
+		).toJSON();
+		expect(CalendarPicker).toMatchSnapshot();
+	});
 
-  it('It renders calendar picker with props', () => {
-    const minDate = new Date(); // Today
-    const maxDate = new Date(2017, 6, 3);
-    const CalendarPicker = renderer.create(
-      <CalenderPicker
-        startFromMonday={true}
-        allowRangeSelection={true}
-        minDate={minDate}
-        maxDate={maxDate}
-        todayBackgroundColor="#f2e6ff"
-        selectedDayColor="#7300e6"
-        selectedDayTextColor="#FFFFFF"
-        onDateChange={() => {}}
-      />
-    ).toJSON();
-    expect(CalendarPicker).toMatchSnapshot();
-  });
+	it('It renders calendar picker with props', () => {
+		const minDate = new Date(2017, 6, 1);
+		const maxDate = new Date(2017, 6, 3);
+		const CalendarPicker = renderer.create(
+			<CalenderPicker
+				startFromMonday={true}
+				allowRangeSelection={true}
+				initialDate={new Date(2017, 11, 31)}
+				minDate={minDate}
+				maxDate={maxDate}
+				todayBackgroundColor="#f2e6ff"
+				selectedDayColor="#7300e6"
+				selectedDayTextColor="#FFFFFF"
+				onDateChange={() => {}}
+			/>
+		).toJSON();
+		expect(CalendarPicker).toMatchSnapshot();
+	});
 
 });
