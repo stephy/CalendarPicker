0213e411056b540b6bee91aeca1a634e1a706afb
Bryan <bryan.mcneil@gmail.com>
2019-01-23 15:17:55 -0400

    added ability to disable day presses

--- CalendarPicker/index.js
+++ CalendarPicker/index.js
@@ -46,6 +46,7 @@ export default class CalendarPicker extends Component {
     scaleFactor: 375,
     enableSwipe: true,
     onDateChange: () => { console.log('onDateChange() not provided') },
+    enableDayPresses: true
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -106,6 +107,7 @@ export default class CalendarPicker extends Component {
   }
 
   handleOnPressDay(day) {
+
     const {
       currentYear,
       currentMonth,
@@ -116,7 +118,12 @@ export default class CalendarPicker extends Component {
     const {
       allowRangeSelection,
       onDateChange,
+      enableDayPresses
     } = this.props;
+
+    if(!enableDayPresses){
+      return;
+    }
 
     const date = moment({year: currentYear, month: currentMonth, day});
 
