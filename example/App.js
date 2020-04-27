a42cbe31b1cd257ee87f71ffca65ea37821541cc
peacechen <github@ghpc.33mail.com>
2020-04-27 01:51:52 -0500

    Update example with customDatesStyles & dayOfWeekStyles.

--- example/App.js
+++ example/App.js
@@ -4,15 +4,40 @@ import {
   Text,
   View
 } from 'react-native';
+import moment from 'moment';
 import CalendarPicker from './CalendarPicker';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    let minDate = moment().subtract(10, 'day');
+    let day = minDate.clone();
+    let customDatesStyles = [];
+    for (let i = 0; i < 30; i++) {
+      customDatesStyles.push({
+        date: day.clone(),
+        // Random colors
+        style: {backgroundColor: '#'+('#00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6)},
+        textStyle: {color: 'black'}, // sets the font color
+        containerStyle: [], // extra styling for day container
+      });
+      day.add(1, 'day');
+    }
+
+    let dayOfWeekStyles = {
+      5: {
+        color: '#00f',
+        fontWeight: 'bold',
+      }
+    };
+
     this.state = {
       selectedStartDate: null,
-      minDate: new Date(2020, 0, 20),
-      maxDate: new Date(2020, 1, 30)
+      minDate,
+      maxDate: moment().add(90, 'day'),
+      customDatesStyles,
+      dayOfWeekStyles,
     };
     this.onDateChange = this.onDateChange.bind(this);
   }
@@ -20,12 +45,12 @@ export default class App extends Component {
   onDateChange(date) {
     this.setState({
       selectedStartDate: date,
-      minDate: new Date(2020, 0, 24),
     });
   }
+
   render() {
     const { selectedStartDate } = this.state;
-    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+    const startDate = selectedStartDate ? selectedStartDate.format('YYYY-MM-DD') : '';
     const initialDate = this.state.minDate;
     return (
       <View style={styles.container}>
@@ -34,10 +59,12 @@ export default class App extends Component {
           initialDate={initialDate}
           minDate={this.state.minDate}
   				maxDate={this.state.maxDate}
+          customDatesStyles={this.state.customDatesStyles}
+          dayOfWeekStyles={this.state.dayOfWeekStyles}
         />
 
         <View>
-          <Text>SELECTED DATE:{ startDate }</Text>
+          <Text>SELECTED DATE:  { startDate }</Text>
         </View>
       </View>
     );
